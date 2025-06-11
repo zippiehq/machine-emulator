4fea7054545cea064fcd1ebf2d55d2cfbd2b7d7c
Carsten Munk <carsten@zippie.com>
2025-06-11 12:35:56 +0200

    fix
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- src/dtb.cpp
+++ src/dtb.cpp
@@ -153,7 +153,18 @@ void dtb_init(const machine_config &c, unsigned char *dtb_start, uint64_t dtb_le
                 fdt.prop_u32_list<2>("interrupts-extended", {PLIC_PHANDLE, plic_irq_id});
                 fdt.end_node();
             }
-            fdt.end_node();
+            if (c.virtio.size() == 0) {
+                for (uint32_t virtio_idx = 0; virtio_idx < 8; ++virtio_idx) { // virtio-mmio
+                    const uint64_t virtio_paddr = PMA_FIRST_VIRTIO_START + (virtio_idx * PMA_VIRTIO_LENGTH);
+                    const uint32_t plic_irq_id = virtio_idx + 1;
+                    fdt.begin_node_num("virtio", virtio_paddr);
+                    fdt.prop_string("compatible", "virtio,mmio");
+                    fdt.prop_u64_list<2>("reg", {virtio_paddr, PMA_VIRTIO_LENGTH});
+                    fdt.prop_u32_list<2>("interrupts-extended", {PLIC_PHANDLE, plic_irq_id});
+                    fdt.end_node();
+                }
+                fdt.end_node();
+            }
         }
 
         { // memory
